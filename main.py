80478f83b06bd245fe155211fbec250be42f9bd1
Erin Godwin <godwin.erin@gmail.com>
2017-01-26 16:47:01 -0600

    Fortune Cookie with fortune, lucky number, and refresh button

--- main.py
+++ main.py
@@ -15,10 +15,34 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+    "I see much code in your future",
+    "Consider eating more fortune cookies",
+    "You have freed the mighty python, now you much release it on the spider's web"
+    ]
+    index= random.randint(0,2)
+    return fortunes[index]
+
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1> Fortune Cookie </h1>"
+        fortune= "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your Fortune: " + fortune
+        fortune_paragraph = '<p>' + fortune_sentence + "</p>"
+
+        lucky_number =  "<strong>" + str(random.randint(1, 100)) + "</strong>"
+        number_sentence = 'Your lucky number: ' + lucky_number
+        number_paragraph = "<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href= '.'><button> Give me another cookie, please! </button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
